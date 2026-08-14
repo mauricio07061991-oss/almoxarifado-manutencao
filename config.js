7a5f4ea4d4ecef86cfe5e3c7566a839ca0a78adf
mauricio07061991-oss <mauricio07061991@gmail.com>
2026-08-14 20:56:13 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.ESTOQUE_CONFIG = {
+window.CONFIGURACAO_DE_ESTOQUE = {
   SUPABASE_URL: "https://hosmreyndhyjvcxxpoxd.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_dBQyCJ4Cocu6xXHF23s0BA_Q5uUJ--t"
 };
